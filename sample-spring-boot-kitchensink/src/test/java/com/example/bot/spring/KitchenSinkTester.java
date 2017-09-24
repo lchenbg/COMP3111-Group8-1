5a8cfb48183f09aa386821964ae517704de16009
Chen Charles <lchenbg@ust.hk>
2017-09-24 23:54:43 +0800

    t1

--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -79,6 +79,6 @@ public class KitchenSinkTester {
 			thrown = true;
 		}
 		assertThat(!thrown).isEqualTo(true);
-		assertThat(result).isEqualTo("");
+		assertThat(result).isEqualTo("Hey, how things going?");
 	}
 }
