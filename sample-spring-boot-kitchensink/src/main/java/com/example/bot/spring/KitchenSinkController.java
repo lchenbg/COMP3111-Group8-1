357528c9ef9865cf12aa9908403b42e5059a5bf3
Chen Charles <lchenbg@ust.hk>
2017-10-27 09:50:52 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -407,6 +407,8 @@ public class KitchenSinkController {
         		userGuideHandler(replyToken, event, text);
         		break;
         	default:
+        		log.info("XXXXXXXXXXXXXXXXXXXXXX");
+        		log.info(currentStage);
         		String msg = "I am deactivated. To reactivate me, please block->unblock me.";
         		this.replyText(replyToken, msg);
         		break;
